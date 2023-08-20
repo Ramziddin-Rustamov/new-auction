c1dcabbaa935b807e3688053e6ae5effd0c1100b
Ramziddin7 <rustamovramziddin7@gmail.com>
2023-08-20 13:11:19 +0200

    adding test

--- resources/views/fallback.blade.php
+++ resources/views/fallback.blade.php
@@ -4,7 +4,7 @@
     <title>Auction</title>
 </head>
 <body>
-    <h1 style="justify-content: center; text-align:center">Sorry, this is just for API. If you have any questions, please contact the developer:</h1>
+    <h1 style="justify-content: center; text-align:center">Sorry, this is just for API. If you have any questions, please contact with our developer:</h1>
     <p style="text-align: center;">
         <a href="mailto:rustamovvramziddin@gmail.com">rustamovvramziddin@gmail.com</a> or
         <a href="#">icoder.rusram.uz</a>
