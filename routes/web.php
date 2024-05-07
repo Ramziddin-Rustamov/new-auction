ed8b73eee6fedd82823bb9cfab7cd72db6c06435
Ramziddin7 <rustamovramziddin7@gmail.com>
2024-05-07 16:24:50 +0200

    a

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use App\Http\Controllers\API\v1\ProductController;
+use App\Http\Controllers\API\v1\VerificationApiController;
+use App\Http\Controllers\Auth\VerificationController;
 use App\Http\Controllers\ComplitedProductsController;
 use App\Http\Controllers\DetailsController;
 use App\Http\Controllers\IndexControler;
@@ -8,6 +10,8 @@ use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [IndexControler::class,'index'])->name("index");
+Route::get('email/verify/{id}', [VerificationApiController::class , 'verify'])->name('verificationapi.verify');
+Route::get('email/resend', [VerificationApiController::class , 'resend'])->name('verificationapi.resend');
 
 Auth::routes([
     'verify'=>true
